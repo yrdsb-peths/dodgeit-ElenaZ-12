0f13e5401b12e2764729c5332a89c2744d6ccb20
ElenaZ-12 <166631491+ElenaZ-12@users.noreply.github.com>
2024-04-22 11:50:23 -0400

    Reset Banana

--- Banana.java
+++ Banana.java
@@ -4,6 +4,24 @@ public class Banana extends Actor
 {
     public void act()
     {
-        move(-3);
+        move(-5);
+        
+        if(getX()<=0)
+        {
+            resetBanana();
+        }
+    }
+    
+    public void resetBanana()
+    {
+        int num = Greenfoot.getRandomNumber(2);
+        if (num==0)
+        {
+            setLocation(600,100);
+        }
+        else
+        {
+            setLocation(600,300);
+        }
     }
 }
